a0aa20558fe9ac91f62d94d3284a2849a28e2769
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-14 19:32:47 +0700

    + Add user and Delete user

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 export default class AddUser extends Component {
+
   state = {
+    id: this.props.data.users[this.props.data.users.length - 1]['id'] + 1,
     name: null,
     email: null,
     phone: null,
     website: null
   };
+
+
 
   handleChange = event => {
     this.setState({
@@ -15,23 +19,28 @@ export default class AddUser extends Component {
   };
 
   handleSubmit = event => {
-      event.preventDefault();
-      console.log(this.state);   
+    let newId = this.state.id + 1;  
+    event.preventDefault();
+      this.setState({
+        id: newId
+      })
+      console.log(this.state);
+      this.props.addUser(this.state);
   }
 
   render() {
     return (
       <div>
         <form className="form" onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" onChange={this.handleChange} />
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" onChange={this.handleChange} />
-          <label htmlFor="phone">Phone:</label>
-          <input type="text" id="phone" onChange={this.handleChange} />
-          <label htmlFor="website">Website:</label>
-          <input type="text" id="website" onChange={this.handleChange} />
-          <button>Submit</button>
+          <label className="label" htmlFor="name">Name:</label>
+          <input className="input" type="text" id="name" onChange={this.handleChange} /><br/>
+          <label className="label" htmlFor="email">Email:</label>
+          <input className="input" type="text" id="email" onChange={this.handleChange} /><br/>
+          <label className="label" htmlFor="phone">Phone:</label>
+          <input className="input" type="text" id="phone" onChange={this.handleChange} /><br/>
+          <label className="label" htmlFor="website">Website:</label>
+          <input className="input" type="text" id="website" onChange={this.handleChange} /><br/>
+          <button className="button is-primary">Submit</button>
         </form>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import 'bulma/css/bulma.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
 
 window.onload = function () {
 
