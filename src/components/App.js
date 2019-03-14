f8dac44b8870c651115ac58ed3ef2eea0b503d03
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-15 00:10:34 +0700

    Complete Todo App with React basic and Materialize CSS Framework

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
 import '../css/styles.scss'
+import Todos from './Todos';
+import AddTodo from '../AddTodo';
 
 export default class App extends Component {  
+  
+  state = {
+    todos: [
+      {
+        id: 1,
+        content: 'Go swimming'
+      },
+      {
+        id: 2,
+        content: 'Grab a coffee'
+      }
+    ]
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let shallowTodos = [...this.state.todos, todo]
+    this.setState({
+      todos: shallowTodos
+    })
+  }
+
+  deleteTodo = (id) => {
+    let todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
-      <div>
-        <p className="title has-text-primary">Todo App</p>
+      <div className="App container">
+        <h2 className="cyan-text">Todo's</h2><br></br>
+        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos} /><br></br>
+        <AddTodo addTodo={this.addTodo} todos={this.state} />
       </div> 
     )
   }
-
 }
