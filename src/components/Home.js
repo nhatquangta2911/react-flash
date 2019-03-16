4a9c395de530cbef63a1a235087bd9cdbaf8eaa7
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-17 00:01:42 +0700

    + Continue Route Params (individual post) , Switch Tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,49 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 class Home extends Component {
-  state = {
-    posts: []
-  };
+   state = {
+      posts: []
+   };
 
-  componentDidMount() {
-    // async, don't know when it'll finish
-    // this method return a Promise
-    // this Promise means: OK! this action will complete at some point in time
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      this.setState({
-        posts: res.data.slice(0, 10)
+   componentDidMount() {
+      // async, don't know when it'll finish
+      // this method return a Promise
+      // this Promise means: OK! this action will complete at some point in time
+      axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+         this.setState({
+            posts: res.data.slice(0, 10)
+         });
       });
-    });
-  }
+   }
 
-  render() {
-    const { posts } = this.state;
-    const postList = posts.length ? (
-      posts.map(post => {
-        return (
-          <div className="post card" key={post.id}>
-            <div className="card-content">
-              <span className="new amber darken-2 badge"></span>
-              <div className="card-title cyan-text">{post.title}</div>
-              <p className="grey-text">{post.body}</p>
-            </div>
-          </div>
-        );
-      })
-    ) : (
-      <div className="center">No posts yet</div>
-    );
+   render() {
+      const { posts } = this.state;
+      const postList = posts.length ? (
+         posts.map(post => {
+            return (
+               <div className="post card" key={post.id}>
+                  <div className="card-content">
+                     <span className="new amber darken-2 badge" />
+                     <Link to={"/" + post.id}>
+                        <div className="card-title cyan-text">{post.title}</div>
+                     </Link>
+                     <p className="grey-text">{post.body}</p>
+                  </div>
+               </div>
+            );
+         })
+      ) : (
+         <div className="center">No posts yet</div>
+      );
 
-    return (
-      <div className="container">
-        <h2 className="center cyan-text lighten-1">Posts</h2>
-        {postList}
-      </div>
-    );
-  }
+      return (
+         <div className="container">
+            <h2 className="center cyan-text lighten-1">Posts</h2>
+            {postList}
+         </div>
+      );
+   }
 }
 
 export default Home;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class Post extends Component {
-    
-    state = {
-        id: null
-    }
+   state = {
+      post: null
+   };
 
-    componentDidMount() {
-        let id = this.props.match.params.post_id
-        this.setState({
-            id
-        })
-    }
+   componentDidMount() {
+      let id = this.props.match.params.post_id;
 
-    render() {
-        return (
-            <div className="container">
-                <p className="grey-text">Route Parameter <span className="amber-text">{this.state.id}</span></p>
+      axios
+         .get("https://jsonplaceholder.typicode.com/posts/" + id)
+         .then(res => {
+            this.setState({
+               post: res.data
+            });
+         });
+   }
+
+   render() {
+      const post = this.state.post ? (
+         <div className="post card">
+            <div className="card-content">
+               <span className="new amber darken-2 badge" />
+               <div className="card-title cyan-text">{this.state.post.title}</div>
+               <p className="grey-text">{this.state.post.body}</p>
             </div>
-        );
-    }
+         </div>
+      ) : (
+         <div className="center">Loading post...</div>
+      );
+
+      return (
+         <div className="container">
+            <br/>
+            {post}
+         </div>
+      );
+   }
 }
