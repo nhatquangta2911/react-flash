d55c1c6cb9b749aad5b44203f3b1cc29606fafc2
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-16 19:11:15 +0700

    + Grab fake dummy data from REST API using Axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+class Home extends Component {
+  state = {
+    posts: []
+  };
 
-const Home = (props) => {
+  componentDidMount() {
+    // async, don't know when it'll finish
+    // this method return a Promise
+    // this Promise means: OK! this action will complete at some point in time
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        posts: res.data.slice(0, 10)
+      });
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span class="new amber darken-2 badge"></span>
+              <div className="card-title cyan-text">{post.title}</div>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+    );
 
     return (
-        <div className="container">
-            <h3 className="center">Home Page</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam dolor possimus, quibusdam rerum quas inventore excepturi architecto soluta provident vel molestiae ducimus beatae reprehenderit explicabo facere praesentium harum maxime quo.</p>
-        </div>
-    )
-
+      <div className="container">
+        <h2 className="center cyan-text lighten-1">Posts</h2>
+        {postList}
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
