a4d97afba2571bdb8bcf655ffb386933b47be861
NhatQuangTa <nhatquangta2911@gmail.com>
2019-03-17 13:58:26 +0700

    + Connect Componet to Redux store to grab data instead of getting from JSONPlacthoder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 class Home extends Component {
-   state = {
-      posts: []
-   };
-
+   /*
+      state = {
+         posts: []
+      };
    componentDidMount() {
       // async, don't know when it'll finish
       // this method return a Promise
       // this Promise means: OK! this action will complete at some point in time
       axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-         this.setState({
+         this.setState({ 
             posts: res.data.slice(0, 10)
          });
       });
    }
+*/
+
+
 
    render() {
-      const { posts } = this.state;
+      console.log(this.props)
+      const { posts } = this.props;
       const postList = posts.length ? (
          posts.map(post => {
             return (
@@ -46,4 +51,10 @@ class Home extends Component {
    }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+   return {
+      posts: state.posts
+   }
+}
+
+export default connect(mapStateToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // require('./css/styles.scss');
-import 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import "jquery";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
 
-window.onload = function () {
+const store = createStore(rootReducer);
 
-  ReactDOM.render(<App />, document.getElementById('root'));
-
+window.onload = function() {
+   ReactDOM.render(
+      <Provider store={store}>
+         <App />
+      </Provider>,
+      document.getElementById("root")
+   );
 };
