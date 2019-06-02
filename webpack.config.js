88fef777d8ee58b8945bf9c8a65a1cc8a743ca1f
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-02 08:16:24 +0700

    Test ENV to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     app: "./src/index.js"
   },
