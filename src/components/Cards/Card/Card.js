a374f97b5b345dadc3a3f350421831dd86676102
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-03 13:43:29 +0700

    Change another

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -37,9 +37,9 @@ export default class Card extends Component {
                <div className="card-item">
                   <h3>{card[0].englishTitle}</h3>
                   <h5>{card[0].vietnameseTitle}</h5>
+                  <p>{card[0].type}</p>
                   <img src={card[0].image} alt={card[0].englishTitle} />
                   <p>{card[0].example}</p>
-                  {/* <p>{card[0].type}</p> */}
                   <h5>{card[0].context}</h5>
                   {/* <p>{card[0].dateCreated}</p> */}
                   {/* <p>{card[0].isRemember}</p> */}
