5c41ffd4f579bd2aefd01cb9c2bde0b3e0d06c87
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-04 20:20:29 +0700

    Resolve Conflict

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import "./css/styles.scss";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Post from "./components/Post";
+import User from "./components/User";
 import Movies from "./components/Movies";
 import Movie from "./components/Movies/Movie";
 import Cards from "./components/Cards/";
 import Card from "./components/Cards/Card";
 import CardsRecent from "./components/CardsRecent";
 import SearchBox from "./components/SearchBox/SearchBox";
-
 export default class App extends Component {
    render() {
       return (
-         <BrowserRouter basename={process.env.PUBLIC_URL}>
+         <BrowserRouter>
             <div className="App">
                <Navbar />
                <SearchBox />
@@ -23,8 +24,14 @@ export default class App extends Component {
                   <Route exact path="/" component={CardsRecent} />
                   <Route path="/about" component={About} />
                   <Route path="/contact" component={Contact} />
+                  <Route path="/posts/:post_id" component={Post} />
+                  <Route path="/users/:user_id" component={User} />
+                  <Route path="/movies" component={Movies} />
+                  <Route path="/movies/:movie_id" component={Movie} />
                   <Route exact path="/cards" component={Cards} />
+                  {/* <Route path="/cards/random" component={Card} /> */}
                   <Route path="/cards/:id" component={Card} />
+
                </Switch>
             </div>
          </BrowserRouter>
