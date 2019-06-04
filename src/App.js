5ca6476e1996e73fe6e952f4aed2a3b49f1710e2
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-04 17:51:25 +0700

    Check Route not working

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Post from "./components/Post";
-import User from "./components/User";
 import Movies from "./components/Movies";
 import Movie from "./components/Movies/Movie";
 import Cards from "./components/Cards/";
@@ -25,14 +23,9 @@ export default class App extends Component {
                   <Route exact path="/" component={CardsRecent} />
                   <Route path="/about" component={About} />
                   <Route path="/contact" component={Contact} />
-                  {/* <Route path="/posts/:post_id" component={Post} /> */}
-                  {/* <Route path="/users/:user_id" component={User} /> */}
-                  {/* <Route path="/movies" component={Movies} /> */}
-                  {/* <Route path="/movies/:movie_id" component={Movie} /> */}
                   <Route exact path="/cards" component={Cards} />
-                  {/* <Route path="/cards/random" component={Card} /> */}
-               </Switch>
                   <Route path="/cards/:id" component={Card} />
+               </Switch>
             </div>
          </BrowserRouter>
       );
