577949216e9dcd60da6a3fde7f136d536d8e9f3c
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-08 10:47:20 +0700

    Change temp responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link, NavLink, withRouter} from 'react-router-dom'
 import jwt from 'jsonwebtoken';
+import styles from './Navbar.scss';
 
 const handleLogout = () => {
     window.localStorage.removeItem('token');
@@ -16,14 +17,13 @@ const Navbar = (props) => {
     return (
        <nav className="nav-extends cyan darken-1">
             <div className="container">
-                <a className="left brand-logo"><span className="amber-text"><b>Shawn</b></span>FLASH</a>
+                <a id="shawn-logo" className="left brand-logo"><span className="amber-text"><b>Shawn</b></span>FLASH</a>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/cards">Flashcards</Link></li> 
                     {/* <Link><Link to="/about">About</Link></Link> */}
                     {!token && <li><Link to="/auth">Login</Link></li>}
                     {token && user && user.name && <li><Link to="/profile">Welcome <b>{user.name}</b></Link></li>}
-                    {token && <li onClick={handleLogout}><Link to="/">Logout</Link></li>}
                 </ul>
             </div>
        </nav>
@@ -36,3 +36,98 @@ const Navbar = (props) => {
 // Apply this properties to this props
 // As React Router wrapped all the component, and Navbar is not in them
 export default withRouter(Navbar)
+
+// import React from "react";
+// import { Link, NavLink, withRouter } from "react-router-dom";
+// import jwt from "jsonwebtoken";
+
+// const handleLogout = () => {
+//    window.localStorage.removeItem("token");
+// };
+
+// const Navbar = props => {
+//    // setTimeout(() => {
+//    //     props.history.push('/about')
+//    // }, 2000)
+//    const token = window.localStorage.getItem("token");
+//    const user = token && jwt.decode(token);
+//    return (
+//       <div>
+//          <nav className=" cyan darken-1">
+//             <div className="nav-wrapper">
+//                <a href="#" className=" brand-logo">
+//                   <span className="amber-text">
+//                      <b>Shawn</b>
+//                   </span>
+//                   FLASH
+//                </a>
+//                {/* <a
+//                   href="#"
+//                   data-target="mobile-demo"
+//                   className="sidenav-trigger"
+//                >
+//                   <i className="material-icons">menu</i>
+//                </a> */}
+//                <ul className="right hide-on-med-and-down">
+//                   <li>
+//                      <Link to="/">Home</Link>
+//                   </li>
+//                   <li>
+//                      <Link to="/cards">Flashcards</Link>
+//                   </li>
+//                   {/* <Link><Link to="/about">About</Link></Link> */}
+//                   {!token && (
+//                      <li>
+//                         <Link to="/auth">Login</Link>
+//                      </li>
+//                   )}
+//                   {token && user && user.name && (
+//                      <li>
+//                         <Link to="/profile">
+//                            Welcome <b>{user.name}</b>
+//                         </Link>
+//                      </li>
+//                   )}
+//                   {token && (
+//                      <li onClick={handleLogout}>
+//                         <Link to="/">Logout</Link>
+//                      </li>
+//                   )}
+//                </ul>
+//             </div>
+//          </nav>
+//          <ul className="sidenav" id="mobile-demo">
+//             <li>
+//                <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//                <Link to="/cards">Flashcards</Link>
+//             </li>
+//             {/* <Link><Link to="/about">About</Link></Link> */}
+//             {!token && (
+//                <li>
+//                   <Link to="/auth">Login</Link>
+//                </li>
+//             )}
+//             {token && user && user.name && (
+//                <li>
+//                   <Link to="/profile">
+//                      Welcome <b>{user.name}</b>
+//                   </Link>
+//                </li>
+//             )}
+//             {token && (
+//                <li onClick={handleLogout}>
+//                   <Link to="/">Logout</Link>
+//                </li>
+//             )}
+//          </ul>
+//       </div>
+//    );
+// };
+
+// // High order function
+// // Wrapping the component => supercharging this component
+// // Apply this properties to this props
+// // As React Router wrapped all the component, and Navbar is not in them
+// export default withRouter(Navbar);
