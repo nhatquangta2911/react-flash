a3da4c6b23451d1240c84d4a6b53bb2d456d0b3a
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-11 12:00:24 +0700

    Inital Chat component

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Chat extends Component {
+   render() {
+      return (
+         <div className="chat-container">
+            <p>Chat room</p>
+         </div>
+      )
+   }
+}
