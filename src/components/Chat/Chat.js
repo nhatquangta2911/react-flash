0279d946a3385f294cf960abac1daf6947461ec5
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-11 17:02:44 +0700

    Finish chat room

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import io from "socket.io-client";
+import styles from './Chat.scss';
 
 export default class Chat extends Component {
+   
+   constructor(props) {
+      super(props);
+      this.state = {
+         message: '',
+         handle: '',
+         output: [],
+         feedback: '',
+         socket: io('https://shawn-movie-rental.herokuapp.com')
+      }
+   }
+
+   componentDidMount() {
+      this.handleSocket();
+   }
+
+   handleSocket = () => {
+      this.state.socket.on('chat', (data) => {
+         this.setState({
+            output: [...this.state.output, {
+               message: data.message,
+               handle: data.handle
+            }],
+            feedback: ''
+         });
+      });
+      this.state.socket.on('typing', (data) => {
+         this.setState({
+            feedback: data
+         })
+      });
+   }
+
+   handleClick = () => {
+      this.state.socket.emit('chat', {
+         message: this.state.message,
+         handle: this.state.handle
+      });
+   };
+
+   handleTyping = () => {
+      this.state.socket.emit('typing', this.state.handle);
+   }
+
+   handleChangeHandle = (event) => {
+      this.setState({
+         handle: event.target.value
+      });
+   }  
+
+   handleChangeMessage = (event) => {
+      this.setState({
+         message: event.target.value
+      });
+   }  
+
    render() {
+      const {message, handle, output, feedback, socket} = this.state;
+      const outputResult = output && output.map(o => (<p><strong>{o.handle}</strong>{o.message}</p>));
+      const feedbackOutput = feedback && (<p><em><b>{feedback}</b> is typing a message...</em></p>);
       return (
-         <div className="chat-container">
-            <p>Chat room</p>
+         <div id="flash-chat">
+            <div id="chat-window">
+               <div id="output">{outputResult}</div>
+            <div id="feedback">
+               {feedbackOutput}
+            </div>
+         </div>
+         <input onChange={this.handleChangeHandle} type="text" id="handle" placeholder="Handle" value={handle}></input>
+         <input onChange={this.handleChangeMessage} onKeyPress={this.handleTyping} type="text" id="message" placeholder="Message" value={message} ></input>
+         <button id="send" onClick={this.handleClick}>Send</button>
          </div>
       )
    }
