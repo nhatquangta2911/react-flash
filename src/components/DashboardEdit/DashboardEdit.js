97e316d484faba2d5bc5c8d48f95d67986e39e68
Nhat Quang Ta <nhatquangta2911@gmail.com>
2019-06-15 10:05:51 +0700

    handle change edit form

--- src/components/DashboardEdit/DashboardEdit.js
+++ src/components/DashboardEdit/DashboardEdit.js
@@ -8,7 +8,14 @@ export default class DashboardEdit extends Component {
       this.state = {
          isLoading: true,
          id: "",
-         card: ""
+         card: {
+            englishTitle: '',
+            vietnameseTitle: '',
+            example: '',
+            context: '',
+            type: '',
+            image: ''
+         }
       };
    }
 
@@ -23,7 +30,14 @@ export default class DashboardEdit extends Component {
          .then(res => {
             this.setState({
                isLoading: false,
-               card: res.data
+               card: {
+                  englishTitle: res.data.englishTitle,
+                  vietnameseTitle: res.data.vietnameseTitle,
+                  example: res.data.example,
+                  context: res.data.context,
+                  type: res.data.type,
+                  image: res.data.image
+               }
             });
             document.title = `Edit Card - ${res.data.englishTitle}`;
          })
@@ -35,15 +49,35 @@ export default class DashboardEdit extends Component {
          .then(res => {
             this.setState({
                isLoading: false,
-               card: res.data
+               card: {
+                  englishTitle: res.data.englishTitle,
+                  vietnameseTitle: res.data.vietnameseTitle,
+                  example: res.data.example,
+                  context: res.data.context,
+                  type: res.data.type,
+                  image: res.data.image
+               }
             });
          })
          .catch(err => {});
       document.title = `Edit Card - ${res.data.englishTitle}`;
    }
 
+   setFormData = (key, value) => {
+      let card = this.state.card;
+      card[key] = value;
+      this.setState({
+         card
+      });
+   } 
+
+   handleSubmit = () => {
+      console.log(this.state.card);
+   }
+
    render() {
       const { isLoading, id, card } = this.state;
+      console.log(card);
       return (
          <div>
             {isLoading && <p className="dashboard-loading">Loading...</p>}
@@ -55,9 +89,11 @@ export default class DashboardEdit extends Component {
                         <input
                            type="text"
                            placeholder="English Title"
-                           id="english-title"
-                           value={card.englishTitle}
-                           onChange={this.handleChange}
+                           id="englishTitle"
+                           value={this.state.card.englishTitle}
+                           onChange={event => {
+                              this.setFormData(event.target.id, event.target.value);
+                           }}
                         />
                      </div>
                   )}
@@ -65,24 +101,60 @@ export default class DashboardEdit extends Component {
                      <input
                         type="text"
                         placeholder="Vietnamese Title"
-                        id="vietnamese-title"
+                        id="vietnameseTitle"
+                        value={this.state.card.vietnameseTitle}
+                        onChange={event => {
+                           this.setFormData(event.target.id, event.target.value);
+                        }}
                      />
                   </div>
                   <div className="dashboard-edit-example">
-                     <input type="text" placeholder="Example" id="example" />
+                     <input
+                         type="text" 
+                         placeholder="Example" 
+                         id="example" 
+                         onChange={event => {
+                           this.setFormData(event.target.id, event.target.value);
+                        }}
+                        value={this.state.card.example}
+                     />
                   </div>
                   <div className="dashboard-edit-context">
-                     <input type="text" placeholder="Context" id="context" />
+                     <input
+                        type="text"
+                        placeholder="Context"
+                        id="context"
+                        onChange={event => {
+                           this.setFormData(event.target.id, event.target.value);
+                        }}
+                        value={this.state.card.context}
+                     />
                   </div>
                   <div className="dashboard-edit-type">
-                     <input type="text" placeholder="Type" id="type" />
+                     <input 
+                        type="text"
+                        placeholder="Type" 
+                        id="type"
+                        onChange={event => {
+                           this.setFormData(event.target.id, event.target.value);
+                        }}
+                        value={this.state.card.type}
+                     />
                   </div>
                   <div className="dashboard-edit-image">
-                     <input type="text" placeholder="Image" id="image" />
+                     <input 
+                        type="text"
+                        placeholder="Image" 
+                        id="image" 
+                        onChange={event => {
+                           this.setFormData(event.target.id, event.target.value);
+                        }}
+                        value={this.state.card.image}
+                     />
                   </div>
                   <div className="dashboard-edit-button">
                      <p className="dashboard-edit-button-cancel">Cancel</p>
-                     <p className="dashboard-edit-button-edit">Edit</p>
+                     <p onClick={this.handleSubmit} className="dashboard-edit-button-edit">Edit</p>
                   </div>
                </div>
             )}
