944d222f0882f5646d3cd31c787a0bcc9d78ab16
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-22 11:40:03 +0700

    Refactor Project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 // require('./css/styles.scss');
-import "jquery";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./reducers/rootReducer";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-const store = createStore(rootReducer);
 
 window.onload = function() {
    ReactDOM.render(
-      <Provider store={store}>
          <BrowserRouter>
             <App />
-         </BrowserRouter>
-      </Provider>,
+         </BrowserRouter>,
       document.getElementById("root")
    );
 };
