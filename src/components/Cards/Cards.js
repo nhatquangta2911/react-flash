9ca1484cc19cf84252d9f8fca1ebdc40572908d5
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-23 23:09:51 +0700

    Very Beautify Cards and home page

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -11,8 +11,8 @@ export default class Cards extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         pageNumber: '',
-         totalPages: '',
+         pageNumber: "",
+         totalPages: "",
          isLoading: true,
          cards: null,
          isError: false
@@ -21,13 +21,13 @@ export default class Cards extends Component {
 
    componentWillMount() {
       this.setState({
-         pageNumber: document.location.href.split('cards/')[1]
+         pageNumber: document.location.href.split("cards/")[1]
       });
    }
 
    componentDidMount() {
       this.setState({
-         pageNumber: document.location.href.split('cards/')[1]
+         pageNumber: document.location.href.split("cards/")[1]
       });
       CardApi.getByPage(this.state.pageNumber)
          .then(res => {
@@ -42,12 +42,12 @@ export default class Cards extends Component {
                isError: true
             });
          });
-      document.title = 'All Flashcards';
+      document.title = "All Flashcards";
    }
 
    componentWillReceiveProps() {
       this.setState({
-         pageNumber: document.location.href.split('cards/')[1]
+         pageNumber: document.location.href.split("cards/")[1]
       });
       CardApi.getByPage(this.state.pageNumber)
          .then(res => {
@@ -62,64 +62,99 @@ export default class Cards extends Component {
                isError: true
             });
          });
-      document.title = 'All Flashcards';
+      document.title = "All Flashcards";
    }
 
    render() {
       const { pageNumber, totalPages, isLoading, cards, isError } = this.state;
       const pagination = (
          <div className="pagination-container">
-            {pageNumber && pageNumber > 1 && <Link to={{ pathname: `${parseInt(this.state.pageNumber) - 1}` }}>
-               <span className="pagination-item">Prev</span>
-            </Link>}
-
-            {pageNumber && pageNumber != 1 && <Link to={{ pathname: `${pageNumber - 5 > 1 ? pageNumber - 5 : 1}` }}><span className="pagination-item">..</span></Link>}
-
-            {pageNumber && totalPages && Array.from(Array(totalPages), (x, index) => index + 1).map(p => (p &&
-               <Link to={p && { pathname: `${p}` }}> 
-                  {p && pageNumber && p == pageNumber && <span className="pagination-item-now-on">{p}</span>}
-                  {p && pageNumber && p != pageNumber && <span className="pagination-item">{p}</span>}
+            {pageNumber && pageNumber > 1 && (
+               <Link
+                  to={{ pathname: `${parseInt(this.state.pageNumber) - 1}` }}
+               >
+                  <span className="pagination-item">Prev</span>
                </Link>
-            )).splice(pageNumber - 1, 5)}
-         
-            {pageNumber && pageNumber <= totalPages - 5 && <Link to={{ pathname: `${parseInt(pageNumber) + 5}` }}><span className="pagination-item">..</span></Link>}
+            )}
 
-            {pageNumber && pageNumber < totalPages && <Link to={{ pathname: `${parseInt(this.state.pageNumber) + 1}` }}>
-               <span className="pagination-item">Next</span>
-            </Link>}
+            {pageNumber && pageNumber != 1 && (
+               <Link
+                  to={{
+                     pathname: `${pageNumber - 5 > 1 ? pageNumber - 5 : 1}`
+                  }}
+               >
+                  <span className="pagination-item">..</span>
+               </Link>
+            )}
+
+            {pageNumber &&
+               totalPages &&
+               Array.from(Array(totalPages), (x, index) => index + 1)
+                  .map(
+                     p =>
+                        p && (
+                           <Link to={p && { pathname: `${p}` }}>
+                              {p && pageNumber && p == pageNumber && (
+                                 <span className="pagination-item-now-on">
+                                    {p}
+                                 </span>
+                              )}
+                              {p && pageNumber && p != pageNumber && (
+                                 <span className="pagination-item">{p}</span>
+                              )}
+                           </Link>
+                        )
+                  )
+                  .splice(pageNumber - 1, 5)}
+
+            {pageNumber && pageNumber <= totalPages - 5 && (
+               <Link to={{ pathname: `${parseInt(pageNumber) + 5}` }}>
+                  <span className="pagination-item">..</span>
+               </Link>
+            )}
+
+            {pageNumber && pageNumber < totalPages && (
+               <Link
+                  to={{ pathname: `${parseInt(this.state.pageNumber) + 1}` }}
+               >
+                  <span className="pagination-item">Next</span>
+               </Link>
+            )}
          </div>
       );
       const listCards =
          cards &&
          cards.map(card => (
             <div className="flashcards-item" key={card._id}>
-              
                {card.isRemember && (
-               <div className="flashcards-is-remember">
-                 <Link to={toCardLink(card._id)}>
-                     <h4 className="hide-item">{card.englishTitle}</h4>
-                     <h5>{card.vietnameseTitle}</h5>
+                  <Link to={toCardLink(card._id)}>
+                     <div className="flashcards-is-remember">
+                        <p className="hide-item card-englishTitle">
+                           {card.englishTitle}
+                        </p>
+                        <p className="card-vietnameseTitle">{card.vietnameseTitle}</p>
+                        {/* <p className="type">{card.type}</p> */}
+                        <img src={card.image} alt={card.englishTitle} />
+                        <p className="hide-item card-example">{card.example}</p>
+                        {/* <p className="hide-item">{card.context}</p> */}
+                     </div>
                   </Link>
-                  <p className="type">{card.type}</p>
-                  <img src={card.image} alt={card.englishTitle} />
-                  <p className="hide-item">{card.example}</p>
-                  <p className="hide-item">{card.context}</p>
-               </div>
                )}
 
                {!card.isRemember && (
-               <div>
-                 <Link to={toCardLink(card._id)}>
-                     <h4 className="hide-item">{card.englishTitle}</h4>
-                     <h5>{card.vietnameseTitle}</h5>
+                  <Link to={toCardLink(card._id)}>
+                     <div className="flashcards-is-not-remembered">
+                        <p className="hide-item card-englishTitle">
+                           {card.englishTitle}
+                        </p>
+                        <p className="card-vietnameseTitle">{card.vietnameseTitle}</p>
+                        {/* <p className="type">{card.type}</p> */}
+                        <img src={card.image} alt={card.englishTitle} />
+                        <p className="hide-item card-example">{card.example}</p>
+                        {/* <p className="hide-item">{card.context}</p> */}
+                     </div>
                   </Link>
-                  <p className="type">{card.type}</p>
-                  <img src={card.image} alt={card.englishTitle} />
-                  <p className="hide-item">{card.example}</p>
-                  <p className="hide-item">{card.context}</p>
-               </div>
                )}
-             
             </div>
          ));
       return (
--- src/components/CardsRecent/CardsRecent.js
+++ src/components/CardsRecent/CardsRecent.js
@@ -37,44 +37,49 @@ export default class Cards extends Component {
       const { isLoading, cards, isError } = this.state;
       const listCards =
          cards &&
-         cards.map(card => (
-            <div className="cards-item" key={card._id}>
-             {card.isRemember && (
-                <div className="flashcards-random-is-remember">
-                 <Link to={{pathname: `/cards/card/${card._id}`}}>
-                  <h4 className="hide-item">{card.englishTitle}</h4>
-                  <h5>{card.vietnameseTitle}</h5>
-               </Link>
-                  <p className="type">{card.type}</p>
-                  <img src={card.image} alt={card.englishTitle} />
-                  <p className="hide-item">{card.example}</p>
-                  <p className="hide-item">{card.context}</p>
-                </div>
-             )}   
-             {!card.isRemember && (
-                <div>
-                 <Link to={{pathname: `/cards/card/${card._id}`}}>              
-                  <h4 className="hide-item">{card.englishTitle}</h4>
-                  <h5>{card.vietnameseTitle}</h5>
-               </Link>
-                  <p className="type">{card.type}</p>
-                  <img src={card.image} alt={card.englishTitle} />
-                  <p className="hide-item">{card.example}</p>
-                  <p className="hide-item">{card.context}</p>
-                </div>
-             )}
-             
-            </div>
+            cards.map(card => (
+               <div className="flashcards-item" key={card._id}>
+                  {card.isRemember && (
+                     <Link to={toCardLink(card._id)}>
+                        <div className="flashcards-is-remember">
+                           <p className="hide-item card-englishTitle">
+                              {card.englishTitle}
+                           </p>
+                           <p className="card-vietnameseTitle">{card.vietnameseTitle}</p>
+                           {/* <p className="type">{card.type}</p> */}
+                           <img src={card.image} alt={card.englishTitle} />
+                           <p className="hide-item card-example">{card.example}</p>
+                           {/* <p className="hide-item">{card.context}</p> */}
+                        </div>
+                     </Link>
+                  )}
+   
+                  {!card.isRemember && (
+                     <Link to={toCardLink(card._id)}>
+                        <div className="flashcards-is-not-remembered">
+                           <p className="hide-item card-englishTitle">
+                              {card.englishTitle}
+                           </p>
+                           <p className="card-vietnameseTitle">{card.vietnameseTitle}</p>
+                           {/* <p className="type">{card.type}</p> */}
+                           <img src={card.image} alt={card.englishTitle} />
+                           <p className="hide-item card-example">{card.example}</p>
+                           {/* <p className="hide-item">{card.context}</p> */}
+                        </div>
+                     </Link>
+                  )}
+               </div>
          ));
       return (
          <div>
          <ParallaxUI image={<img id="parallax-image" src="https://images.template.net/wp-content/uploads/2016/11/16135851/Cartoon-Gif-Animated1.gif"/>} />
          {/* <div>4 Random Cards</div> */}
+         <p className="super-title">Make your own cards <span><img src="https://img.icons8.com/cotton/2x/bank-card-back-side.png"/></span></p>
          <div className="cards-page-container">
             {isLoading && <Loading message="Loading 4 random cards..." />}
             {listCards}
          </div>
-         <ParallaxUI image={<img src="https://img1.akspic.com/image/123021-geometric_shape-cube-blue-triangle-light_blue-3840x2160.jpg"/>} />         
+         <ParallaxUI image={<img id="parallax-image" src="https://images.template.net/wp-content/uploads/2016/11/16135851/Cartoon-Gif-Animated1.gif"/>} />    
          </div>
       );
    }
