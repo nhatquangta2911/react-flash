d0e92483b7f7c13c95afa2bfbc6f14443f9d8cf3
NhatQuangTa <nhatquangta2911@gmail.com>
2019-06-29 20:20:15 +0700

    Beautify comments sections

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,30 +1,32 @@
 import styles from "./LoginForm.scss";
-import React, { Component } from "react";
-import {Redirect} from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Redirect } from "react-router-dom";
 import AuthApi from "../../api/AuthApi";
 import { Link } from "react-router-dom";
 import Toast from "../Toast/Toast";
+import Loading from "../Loading";
 // import Cookies from "universal-cookie";
 export default class LoginForm extends Component {
    constructor(props) {
       super(props);
       this.state = {
+         isLoading: false,
          email: "",
          password: ""
       };
    }
 
    componentDidMount() {
-      document.title = 'Login'
+      document.title = "Login";
    }
-   
+
    componentDidUpdate() {
       // document.title = 'Login'
    }
 
    // componentDidMount() {
    //    const token = window.localStorage.getItem('token');
-   //    if(token) { 
+   //    if(token) {
    //       this.setState({
    //          isLogin: true,
    //          email: token.email
@@ -45,11 +47,14 @@ export default class LoginForm extends Component {
 
    //TODO: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2VkNDliNGQ2NjNkNTQ4YmNlYTY3ZjAiLCJlbWFpbCI6InNoYXduLmFkbWluQGdtYWlsLmNvbSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU1OTgzMDE1NCwiZXhwIjoxNTU5ODMzNzU0fQ.iV462ANax4u4NILpF5NIIqc8fUk7LNMaTz8Zryx-BP4
    onSubmit = event => {
+      this.setState({
+         isLoading: true
+      });
       event.preventDefault();
       AuthApi.auth(this.state)
          .then(res => {
-            Toast.success('Logged in');
-            window.localStorage.setItem('token', res.data);
+            Toast.success("Logged in");
+            window.localStorage.setItem("token", res.data);
             this.props.history.push("/profile");
          })
          .catch(err => {
@@ -62,32 +67,43 @@ export default class LoginForm extends Component {
    };
 
    render() {
+      const { isLoading } = this.state;
       return (
-         <div className="login-form-containerr">
-            <p>Log in</p>
-            <form onSubmit={this.onSubmit}>
-               <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter email"
-                  autoComplete="new-email"
-                  value={this.state.email}
-                  onChange={this.handleInputChange}
-                  required
+         <Fragment>
+            {isLoading && <Loading message="Loading..."/>}
+            <div className="login-form-containerr">
+               <p>Log in</p>
+               <form onSubmit={this.onSubmit}>
+                  <input
+                     type="email"
+                     name="email"
+                     placeholder="Enter email"
+                     autoComplete="new-email"
+                     value={this.state.email}
+                     onChange={this.handleInputChange}
+                     required
                   />
-               <input
-                  type="password"
-                  name="password"
-                  autoComplete="new-password"
-                  placeholder="Enter password"
-                  value={this.state.password}
-                  onChange={this.handleInputChange}
-                  required
+                  <input
+                     type="password"
+                     name="password"
+                     autoComplete="new-password"
+                     placeholder="Enter password"
+                     value={this.state.password}
+                     onChange={this.handleInputChange}
+                     required
                   />
-                  <input onClick={this.forceUpdate} className="btn-submit" type="submit" value="Submit" />
-            </form>
-            <Link to="/register"><p className="register-title">Not registered yet?</p></Link>
-         </div>
+                  <input
+                     onClick={this.forceUpdate}
+                     className="btn-submit"
+                     type="submit"
+                     value="Submit"
+                  />
+               </form>
+               <Link to="/register">
+                  <p className="register-title">Not registered yet?</p>
+               </Link>
+            </div>
+         </Fragment>
       );
    }
 }
